ee27a608b19aa08ec52d763abdafa087da26cd94
Pers Thanachot <78893367+PersThanachot@users.noreply.github.com>
2022-02-20 10:21:04 +0700

    Create UpdateR.R

---
+++ UpdateR.R
@@ -0,0 +1,17 @@
+#Check version R
+version
+
+if(!require(installr)) #True = Don't Have , False = Have 
+  {
+    install.packages("installr"); #If True Dowload packages
+    library(installr) #If False use Libeary
+  } 
+
+#Update Version R
+updateR() 
+
+#Check version Rstudio = Help > About Rstudio
+#Update version Rstudio = Help > Check for Updates
+
+#Update all packages
+update.packages(ask = FALSE)
